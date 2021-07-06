55e78e8fc1fbd3465784d6e3fcffddbb90a84233
Renato Oda <renato.oda2@gmail.com>
2021-07-05 22:19:59 -0300

    fix: Pass args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-ibmcloud login --apikey "$4" -r "$5"
-ibmcloud cr region-set "$5"
+ibmcloud login --apikey "$5" -r "$6"
+ibmcloud cr region-set "$6"
 ibmcloud cr login
 
 ibmcloud cos download --bucket $1 --key $2 $2
 
-helm update $3 $2 --set=image.tag=$4
+helm upgrade $3 $2 --set=image.tag=$4
